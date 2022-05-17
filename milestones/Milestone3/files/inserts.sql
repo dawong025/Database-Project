2725194391dc65446e6e39b41a06adc174280f31
dawong025 <89830600+dawong025@users.noreply.github.com>
2022-05-17 04:21:24 -0700

    Added revised databasemodel and inserts, other 2 files are the same as m2

---
+++ milestones/Milestone3/files/inserts.sql
@@ -0,0 +1,391 @@
+SET SQL_SAFE_UPDATES = 0; 
+-- USE ATMManagementDB;
+
+INSERT INTO Bank (bank_id, name, address)
+VALUES
+(1, "Chase", "128 Taraval Ave"),
+(2, "Wells Fargo", "512 San Bruno Ave"),
+(3, "Bank of the West", "54 California Street");
+
+INSERT INTO GeneralUser (general_user_id, name, email, ssn, address)
+VALUES
+(1, "Steph Curry", "splashfather@gmail.com", 1, "1 Warriors Way"),
+(2, "Samuel L. Jackson", "imMaceWindu@yahoo.com", 2, "124 Coruscant St"),
+(3, "Will Smith", "catchThemHands@oscars.com", 3, "2 Academy Place"),
+(4, "Rose Park", "rosesarerosie@gmail.com", 4, "397 Hapjeong"), 
+(5, "Ed Sheeran", "edSheeran@yahoo.com", 5, "37 Red Ave"), 
+(6, "Mikasa Ackerman", "mackerman@outlook.com", 6,  "572 Island Way"),
+(7, "Trevor Grant", "trevG@gmail.com", 7, "732 Belmont St"),
+(8, "Jordan Poole", "pooleparty@yahoo.com", 8,  "2 Warriors Way"),
+(9, "Draymond Green", "iShouldHaveBeenDPOY@gmail.com", 9, "3 Warriors Way"),
+(10, "Klay Thompson", "toasterKlay@yahoo.com", 10, "4 Warriors Way");
+
+INSERT INTO BankAccount (bank_account_id, bank_id, general_user_id, account_number)
+VALUES
+(1, 1, 1, 1),
+(2, 2, 2, 2),
+(3, 3, 3, 3),
+(4, 3, 4, 4),
+(5, 2, 5, 5),
+(6, 1, 6, 6),
+(7, 3, 7, 7),
+(8, 2, 8, 8),
+(9, 1, 9, 9),
+(10, 3, 10, 10);
+
+INSERT INTO CheckingAccount(checking_account_id, bank_account_id, balance, fees)
+VALUES
+(1, 1, 100, 0),
+(2, 2, 400, 1),
+(3, 3, 100000, 4),
+(4, 4, 62413, 0),
+(5, 5, 54, 1);
+
+INSERT INTO SavingsAccount(savings_account_id, bank_account_id, balance, interest_rate)
+VALUES
+(1, 1, 3000, 2),
+(2, 2, 5234, 3),
+(3, 3, 45301, 1),
+(4, 4, 3214, 0);
+
+INSERT INTO Owner (owner_id, name, last_name)
+VALUES
+(42, "Harrison Wells", "Fargo"),
+(5321, "Chase Samuel", "McLaughlin"),
+(73261, "Westly Van", "Duke"),
+(312, "Bills",  "McBillington");
+
+INSERT INTO Owns (owns_id, bank_id, owner_id)
+VALUES
+(1, 1, 42),
+(2, 2, 5321),
+(3, 3, 73261),
+(4, 3, 312);
+
+INSERT INTO Affiliate (affiliate_id, name, contract)
+VALUES
+(42027, "Selena Gomez", "$5000 for each appearance/commercial"),
+(3011, "Ewan McGregor", "$2500 for each appearance/commercial"),
+(7, "Jeremy Lin", "$1000 for each appearance/commercial"),
+(674, "Irene Bae", "$3500 for each appearance/commercial");
+
+INSERT INTO Affiliations (affiliation_id, affiliate_id, bank_id)
+VALUES
+(1, 42027, 1),
+(2, 3011, 2),
+(3, 7, 3),
+(4, 674, 3);
+
+INSERT INTO Employee (employee_id, name, email, bank_id, isAdmin)
+VALUES
+(31130, "Raymond Gonzalez", "rgonz@chase.com", 1, 0),
+(15, "Betty Tress", "btress@wellsfargo.com", 2, 1),
+(4232, "Charles Knight", "cknight2@west.com", 3, 0),
+(3511, "Whitney Williams", "wwilliams3@chase.com", 1, 1),
+(2330, "Jennifer Ngyuen", "jnguyen5@west.com", 3, 1),
+(7239, "Jason Brigs", "jbrigs@wells.com", 2, 0),
+(7240, "Elton Brigs", "ebrigs@wellsfargo.com", 2, 0),
+(6338, "Kenny Wong", "kwong9@chase.com", 1, 0),
+(532, "Anna Blouse", "ablouse3@west.com", 2, 0),
+(7428, "Jeff Grimes", "jgrimes@wellsfargo.com", 3, 0),
+(9759, "Eren Chavez", "echavez71@chase.com", 3, 0),
+(9057, "Sasha Blakeman", "sblakeman@west.com", 3, 0),
+(87041, "Levi Samuel", "lsamuel@chase.com", 1, 0),
+(72, "Ryan Reynolds", "rreynolds@wellsfargo.com", 2, 0),
+(6954, "Oscar Thompson", "othompson3@wellsfargo.com", 2, 0),
+(5214, "Chris Smith", "csmith@chase.com", 1, 0),
+(7832, "Riley Evans", "revans24@west.com", 1, 0),
+(811, "Charlie Gibens", "cgibens1@chase.com", 1, 0);
+
+INSERT INTO Admin (admin_id, employee_id, privileges)
+VALUES
+(1, 31130, "ATM management, permissions"),
+(2, 15, "ATM management, verification"),
+(3, 3511, "ATM management, roles, permissions"),
+(4, 2330, "ATM management, roles");
+
+INSERT INTO Security (security_id, employee_id, privileges)
+VALUES
+(1, 7239, "Locking the doors"),
+(2, 7240, "Guarding the vault"),
+(3, 6338, "Camera duty");
+
+INSERT INTO Analyst(analyst_id, employee_id, privileges)
+VALUES
+(1, 532, "Analyze bank transactions"),
+(2, 7428, "Analyze login sessions"),
+(3, 9759, "Analyze bank transactions");
+
+INSERT INTO Consultant(consultant_id, employee_id, privileges)
+VALUES
+(1, 9057, "Consult clients"),
+(2, 87041, "Consult customers"),
+(3, 72, "Consult agents");
+
+INSERT INTO Teller (teller_id, employee_id, privileges)
+VALUES
+(1, 6954, "Record transactions"),
+(2, 5214, "Count currency"),
+(3, 7832, "Promote services");
+
+INSERT INTO Permissions (permissions_id, admin_id, type, bank_account_id)
+VALUES
+(1, 1, "Access Loans", 1),
+(2, 2, "Access Checking Account", 1),
+(3, 3, "Access Savings Account", 2),
+(4, 4, "No permissions", 3);
+
+INSERT INTO Manages (manage_id, admin_id, permission_id)
+VALUES
+(1, 1, 1), 
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4);
+
+INSERT INTO AccountPermissions(account_perm_id, bank_account_id, permission_id)
+VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4);
+
+INSERT INTO Manufacturer (manufacturer_id, name, address)
+VALUES
+(1, "LG Display", "2 LG Way"),
+(2, "Sony LLD", "500 Industrial Way"),
+(3, "Samsan Tech", "324 Startup Ave"),
+(4, "TechEverything", "653 Expanse Way");
+
+INSERT INTO SecurityAlarm(security_alarm_id, manufacturer_id, alarm_range)
+VALUES
+(1, 3, 5),
+(2, 2, 4),
+(3, 2, 3);
+
+INSERT INTO Informs (informs_id, security_id, security_alarm_id)
+VALUES
+(1, 1, 1),
+(2, 2, 2), 
+(3, 3, 3);
+
+INSERT INTO ATMMachine (atm_machine_id, status, bank_id, admin_id)
+VALUES
+(1, "working", 1, 1),
+(2, "working", 2, 2),
+(3, "not_working", 3, 3);
+
+INSERT INTO Languages (language_id, language_name, country)
+VALUES
+(1, "English", "United States"),
+(2, "Chinese", "China"),
+(3, "Korean", "Korea"),
+(4, "Spanish", "Spain");
+
+INSERT INTO Supports (support_id, language_id, atm_machine_id)
+VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 1);
+
+INSERT INTO Loans(loan_id, type, interest_type, bank_id, bank_account_id, terms_conditions)
+VALUES
+(1, "Personal", "Flat", 1, 1, "BORROWER will make payment to LENDER in one payment of $100"),
+(2, "Business", "Fixed", 2, 2, "As collateral for repayment of Loan Amount, BORROWER agrees to put forth a total of 10 Tesla (TSLA) shares"),
+(3, "Mortgage", "Variable", 3, 3, "The Borrower has the right to pay back the loan in-full or make additional
+payments, at any time, without penalty");
+
+INSERT INTO Devices (device_id, manufacturer_id, type, status, atm_machine_id, made)
+VALUES
+(1, 1, "Screen", "Working", 1, now()),
+(2, 2, "Sound", "Working", 1, now()),
+(3, 3, "Scanner", "Not working", 1, now()),
+(4, 3, "I/O", "Working", 1, now());
+
+INSERT INTO Speaker(speaker_id, manufacturer_id, status, device_id)
+VALUES
+(1, 2, "Working", 2),
+(2, 2, "NotWorking", 2),
+(3, 2, "Working", 2),
+(4, 1, "Working", 4);
+
+INSERT INTO CardReader(card_reader_id, manufacturer_id, status, device_id)
+VALUES
+(1, 3, "Working", 3),
+(2, 3, "NotWorking", 3),
+(3, 3, "Working", 3);
+
+INSERT INTO NFCScanner(nfc_scanner_id, manufacturer_id, status, device_id)
+VALUES
+(1, 3, "NotWorking", 3),
+(2, 3, "Working", 3),
+(3, 3, "NotWorking", 3);
+
+INSERT INTO Keypad(keypad_id, manufacturer_id, status, device_id)
+VALUES 
+(1, 3, "Working", 4),
+(2, 3, "Working", 4),
+(3, 3, "NotWorking", 4);
+
+INSERT INTO OLEDScreen(oled_screen_id, manufacturer_id, status, device_id)
+VALUES
+(1, 3, "NotWorking", 4),
+(2, 3, "NotWorking", 4),
+(3, 3, "NotWorking", 4);
+
+INSERT INTO DepositCheckSlot (check_slot_id, manufacturer_id, status, device_id)
+VALUES
+(1, 3, "Working", 4),
+(2, 3, "Working", 4),
+(3, 3, "Working", 4);
+
+INSERT INTO CashDispenser (cash_dispenser_id, manufacturer_id, status, device_id)
+VALUES
+(1, 3, "NotWorking", 4),
+(2, 3, "NotWorking", 4),
+(3, 3, "NotWorking", 4);
+
+INSERT INTO ReceiptPrinter(receipt_printer_id, manufacturer_id, status, device_id)
+VALUES
+(1, 3, "Working", 4),
+(2, 3, "NotWorking", 4),
+(3, 3, "NotWorking", 4);
+
+INSERT INTO VerifiedUser (verified_user_id, general_user_id, privileges, oled_screen_id, keypad_id)
+VALUES
+(1, 1, 4, 1, 1),
+(2, 2, 3, 2, 2),
+(3, 3, 5, 3, 3),
+(4, 8, 3, 1, 1),
+(5, 9, 2, 1, 1);
+
+INSERT INTO VerifiedDevices (verified_devices_id, verified_user_id, device_type)
+VALUES
+(1, 1, "Android Phone"),
+(2, 2, "iPhone"),
+(3, 3, "HP Probook");
+
+INSERT INTO Notifications (notification_id, message, timestamp, verified_device_id, security_alarm_id)
+VALUES
+(1, "Account has been temporarily locked out", now(), 1, 1),
+(2, "Account has been deducted $100", now(), 1, 2),
+(3, "Account has paid off this month's balance", now(), 2, 2),
+(4, "Account has been logged into", now(), 4, 2),
+(5, "Account has been terminated", now(), 5, 2);
+
+INSERT INTO Received(received_id, verified_device_id, notification_id)
+VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3);
+
+INSERT Logins (login_id, verified_user_id, data, logged_in, atm_machine_id)
+VALUES
+(1, 1, "User has logged into the ATM machine", now(), 1),
+(2, 2, "User has logged into the ATM machine", now(), 2),
+(3, 3, "User has logged into the ATM machine", now(), 3);
+
+INSERT INTO Verifies(verifies_id, keypad_id, NFC_scanner_id, card_reader_id, bank_account_id, verified_at)
+VALUES
+(1, 1, 1, 1, 1, now()),
+(2, 2, 2, 2, 2, now()),
+(3, 3, 3, 3, 3, now());
+
+INSERT INTO VerificationMethod(verification_method_id, verified_user_id, verification_type)
+VALUES
+(1, 1, "Card"),
+(2, 1, "DigitalWallet"),
+(3, 2, "Card");
+
+INSERT INTO Card (card_id, type, CVV, zip, exp_date, verification_method_id)
+VALUES
+(1, "Visa", 92, 92113, now(), 1),
+(2, "American Express", 45, 88112, now(), 1),
+(3, "MasterCard", 38, 82314, now(), 1),
+(4, "Visa", 45, 83929, now(), 3),
+(5, "American Express", 56, 64231, now(), 3),
+(6, "MasterCard", 57, 72913, now(), 1);
+
+INSERT INTO DebitCard(debit_card_number, card_id, CVV, zip, exp_date)
+VALUES
+(1, 1, 92, 92113, now()),
+(2, 2, 45, 88112, now()),
+(3, 3, 38, 82314, now());
+
+INSERT INTO CreditCard(credit_card_number, card_id, cvv, zip, exp_date)
+VALUES
+(1, 4, 45, 83929, now()),
+(2, 5, 56, 64231, now()),
+(3, 6, 57, 72913, now());
+
+INSERT INTO DigitalWallet(wallet_id, card_number, type, verification_method_id)
+VALUES
+(1, 1, "Venmo", 2),
+(2, 2, "Zelle", 2),
+(3, 3, "CashApp", 3);
+
+INSERT INTO Scans(scans_id, card_id, card_reader_id, nfc_scanner_id, scanned)
+VALUES
+(1, 1, 1, 1, now()),
+(2, 2, 2, 2, now()),
+(3, 3, 3, 3, now());
+
+INSERT INTO Checks(check_id, verified_user_id, amount, check_slot_id)
+VALUES
+(1, 1, 5000, 1),
+(2, 2, 3541, 2),
+(3, 3, 5401, 3);
+
+INSERT INTO Currency(currency_id, type, amount, verified_user_id, cash_dispenser_id)
+VALUES
+(1, "Dollars", 5, 1, 1),
+(2, "Cents", 10, 1, 2),
+(3, "Canadian Dollars", 5, 1, 3);
+
+INSERT INTO Transactions(transactions_id, amount, atm_machine_id, bank_id, receipt_printer_id)
+VALUES
+(1, 5, 1, 1, 1),
+(2, 10, 2, 2, 2),
+(3, 15, 3, 3, 3);
+
+INSERT INTO Payment(payment_id, transaction_id, balance)
+VALUES
+(1, 1, 5),
+(2, 1, 5),
+(3, 1, 5);
+
+INSERT INTO PaysOff(pays_off_id, payment_id, loan_id)
+VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3);
+
+INSERT INTO Transfer(transfer_id, transaction_id, balance)
+VALUES
+(1, 2, 10),
+(2, 2, 10),
+(3, 2, 10);
+
+INSERT INTO CashWithdrawal(cash_withdrawal_id, transaction_id, balance)
+VALUES
+(1, 2, 15),
+(2, 2, 15),
+(3, 2, 15);
+
+INSERT INTO CashDeposit(cash_deposit_id, transaction_id, balance)
+VALUES
+(1, 2, 15),
+(2, 2, 15),
+(3, 2, 15);
+
+INSERT INTO CheckDeposit(check_deposit_id, transaction_id, balance, check_slot_id)
+VALUES
+(1, 2, 20, 1),
+(2, 2, 20, 1),
+(3, 2, 20, 1);
+
+INSERT INTO CashProcess(cash_process_id, cash_withdrawal_id, cash_deposit_id, cash_dispenser_id)
+VALUES
+(1, 1, 1, 1),
+(2, 2, 2, 2),
+(3, 3, 3, 3);
---
+++ milestones/Milestone3/files/tests.sql
@@ -0,0 +1,239 @@
+-- Script name: tests.sql
+       -- Author:      Darren Wong
+       -- Purpose:     test the integrity of this database system
+       
+SET SQL_SAFE_UPDATES = 0; 
+USE ATMManagementDB;
+
+
+
+-- 1. Bank
+DELETE FROM Bank WHERE bank_id = 3;
+UPDATE Bank SET address = "55 Powell Street" WHERE name = "Wells Fargo";
+
+-- 2. General User
+DELETE FROM GeneralUser WHERE general_user_id = 3;
+UPDATE GeneralUser SET email = "iAmNickFury@yahoo.com" WHERE name = "Samuel L. Jackson";
+
+-- 3. Bank Account
+DELETE FROM BankAccount WHERE bank_account_id = 10;
+UPDATE BankAccount SET account_number = 15 WHERE bank_account_id = 5;
+
+-- 4. CheckingAccount
+DELETE FROM CheckingAccount WHERE checking_account_id = 5;
+UPDATE CheckingAccount SET fees = 4 WHERE checking_account_id = 4;
+
+-- 5. SavingsAccount
+DELETE FROM SavingsAccount WHERE savings_account_id = 4;
+UPDATE SavingsAccount SET interest_rate = 6 WHERE savings_account_id = 3;
+
+-- 6. Owner
+DELETE FROM Owner WHERE owner_id = 312;
+UPDATE Owner SET last_name = "McCown" WHERE owner_id = 42;
+
+-- 7. Owns
+DELETE FROM Owns WHERE owns_id = 4;
+UPDATE Owns SET bank_id = 2 WHERE owns_id = 3;
+
+-- 8. Affiliate 
+DELETE FROM Affiliate WHERE affiliate_id = 7;
+UPDATE Affiliate SET name = "Hayden Christensen" WHERE affiliate_id = 3011;
+
+-- 9. Affiliations
+DELETE FROM Affiliations WHERE affiliation_id = 2;
+UPDATE Affiliations SET affiliate_id = 674 WHERE affiliation_id = 1;
+
+-- 10. Employee
+DELETE FROM Employee WHERE employee_id = 811;
+UPDATE Employee SET name = "Riley House" WHERE employee_id = 7832;
+
+-- 11. Admin
+DELETE FROM Admin WHERE admin_id = 1;
+
+
+UPDATE Admin SET privileges = "ATM management, roles, permissions" WHERE admin_id = 4;
+
+-- 12. Security
+DELETE FROM Security WHERE security_id = 3;
+UPDATE Security SET privileges = "Locking the doors, Guarding the vault" WHERE security_id = 2;
+
+-- 13. Analyst
+DELETE FROM Analyst WHERE analyst_id = 3;
+UPDATE Analyst SET privileges =  "Analyze login sessions and bank transactions" WHERE analyst_id = 2;
+
+-- 14. Consultant
+DELETE FROM Consultant WHERE consultant_id = 3;
+UPDATE Consultant SET privileges = "Consult customers and clients" WHERE consultant_id = 2;
+
+-- 15. Teller
+DELETE FROM Teller WHERE teller_id = 3;
+UPDATE Teller SET privileges = "Record transactions and count currency" WHERE teller_id = 2;
+
+-- 16. Permissions
+DELETE FROM Permissions WHERE permissions_id = 4;
+UPDATE Permissions SET type = "Admin access" WHERE permissions_id = 3;
+
+-- 17. Manages
+DELETE FROM manages WHERE manage_id = 4;
+UPDATE manages SET admin_id = 2 WHERE manage_id = 3;
+
+-- 18. AccountPermissions
+DELETE FROM AccountPermissions WHERE account_perm_id = 2;
+UPDATE AccountPermissions SET bank_account_id = 5 WHERE account_perm_id = 3;
+
+-- 19. Manufacturer
+DELETE FROM Manufacturer WHERE manufacturer_id = 4;
+UPDATE Manufacturer SET name = "Sony" WHERE manufacturer_id = 2;
+
+-- 20. Security Alarm
+DELETE FROM SecurityAlarm WHERE security_alarm_id = 3;
+UPDATE SecurityAlarm SET manufacturer_id = 1 WHERE security_alarm_id = 2;
+
+-- 21. Informs
+DELETE FROM Informs WHERE informs_id = 2;
+UPDATE Informs SET security_id = 2 WHERE informs_id = 1;
+
+-- 22. ATMMachine
+DELETE FROM ATMMachine WHERE atm_machine_id = 3;
+UPDATE ATMMachine SET status = "Not working" WHERE atm_machine_id = 2;
+
+-- 23. Languages
+DELETE FROM Languages WHERE language_id = 3;
+UPDATE Languages SET country = "Spain, LatinAmerica" WHERE language_id = 4; 
+
+-- 24. Supports
+DELETE FROM Supports WHERE support_id = 4;
+UPDATE Supports SET language_id = 2 WHERE support_id = 1;
+
+-- 25. Loans
+DELETE FROM Loans WHERE loan_id = 3;
+
+
+UPDATE Loans SET type = "Auto" WHERE loan_id = 2;
+
+-- 26. Devices
+DELETE FROM Devices WHERE device_id = 2;
+UPDATE Devices SET type = "ETC." WHERE device_id = 4;
+
+-- 27. Speaker
+UPDATE Speaker SET status = "Not working" WHERE speaker_id = 4;
+DELETE FROM Speaker WHERE speaker_id = 4;
+
+-- 28. CardReader
+DELETE FROM CardReader WHERE card_reader_id = 3;
+UPDATE CardReader SET status = "Working" WHERE card_reader_id = 2;
+
+-- 29. NFCScanner
+DELETE FROM NFCScanner WHERE nfc_scanner_id = 3;
+UPDATE NFCScanner SET status = "Not working" WHERE nfc_scanner_id = 2;
+
+-- 30. Keypad
+DELETE FROM Keypad WHERE keypad_id = 3;
+UPDATE Keypad SET status = "Not working" WHERE keypad_id = 2;
+
+-- 31. OLEDScreen
+DELETE FROM OLEDScreen WHERE oled_screen_id = 3;
+UPDATE OLEDScreen SET status = "Working" WHERE oled_screen_id = 2;
+
+-- 32. DepositCheckSlot
+DELETE FROM DepositCheckSlot WHERE check_slot_id = 3;
+UPDATE DepositCheckSlot SET status = "Not working" WHERE check_slot_id = 2;
+
+-- 33. CashDispenser
+DELETE FROM CashDispenser WHERE cash_dispenser_id = 3;
+UPDATE CashDispenser SET status = "Working" WHERE cash_dispenser_id = 2;
+
+-- 34. ReceiptPrinter
+DELETE FROM ReceiptPrinter WHERE receipt_printer_id = 3;
+UPDATE ReceiptPrinter SET status = "Working" WHERE receipt_printer_id = 2;
+
+-- 35. VerifiedUser
+DELETE FROM VerifiedUser WHERE verified_user_id = 1;
+UPDATE VerifiedUser SET privileges = 2 WHERE verified_user_id = 2;
+
+-- 36. VerifiedDevices
+UPDATE VerifiedDevices SET device_type = "iPad" WHERE verified_devices_id = 4;
+DELETE FROM VerifiedDevices WHERE verified_devices_id = 4;
+
+-- 37. Notifications
+UPDATE Notifications SET message = "hi" WHERE notification_id = 5;
+DELETE FROM Notifications WHERE notification_id = 5;
+
+-- 38. Received
+UPDATE Received SET verified_device_id = 4 WHERE received_id = 4;
+DELETE FROM Received WHERE received_id = 2;
+
+-- 39. Logins
+UPDATE Logins SET data = "New Message" WHERE login_id = 2;
+DELETE FROM Logins WHERE login_id = 2;
+
+-- 40. Verifies
+DELETE FROM Verifies WHERE verifies_id = 1;
+UPDATE Verifies SET nfc_scanner_id = 2 WHERE verifies_id = 2;
+
+-- 41. VerificationMethod
+UPDATE VerificationMethod SET verification_type = "Digital Wallet" WHERE verification_method_id = 3;
+DELETE FROM VerificationMethod WHERE verification_method_id = 3;
+
+-- 42. Card
+DELETE FROM Card WHERE card_id = 6;
+UPDATE CARD SET CVV = 96 WHERE card_id = 5;
+
+-- 43. DebitCard
+DELETE FROM DebitCard WHERE debit_card_number = 3;
+UPDATE DebitCard SET CVV = 52 WHERE debit_card_number = 2;
+
+-- 44. CreditCard
+DELETE FROM CreditCard WHERE credit_card_number = 1;
+UPDATE CreditCard SET CVV = 72 WHERE credit_card_number = 1;
+
+-- 45. DigitalWallet
+DELETE FROM DigitalWallet WHERE wallet_id = 3;
+UPDATE DigitalWallet SET type = "paypal" WHERE wallet_id = 2;
+
+-- 46. Scans
+DELETE FROM Scans WHERE scans_id = 3;
+UPDATE Scans SET nfc_scanner_id = 2 WHERE scans_id = 1;
+
+-- 47. Checks
+DELETE FROM Checks WHERE check_id = 3;
+UPDATE Checks SET amount = 354 WHERE check_id = 2;
+
+-- 48. Currency
+DELETE FROM Currency WHERE currency_id = 3;
+UPDATE Currency SET amount = 20 WHERE currency_id = 2;
+
+-- 49. Transactions
+UPDATE Transactions SET amount = 10 WHERE transactions_id = 3;
+DELETE FROM Transactions WHERE transactions_id = 3;
+
+-- 50. Payment
+UPDATE Payment SET balance = 10 WHERE payment_id = 1;
+
+ DELETE FROM Payment WHERE payment_id = 1;
+
+
+-- 51. PaysOff
+UPDATE PaysOff SET payment_id = 2 WHERE pays_off_id = 2;
+DELETE FROM PaysOff WHERE pays_off_id = 2;
+
+-- 52. Transfer
+DELETE FROM Transfer WHERE transfer_id = 3;
+UPDATE Transfer SET balance = 4 WHERE transfer_id = 2;
+
+-- 53. CashWithdrawal
+UPDATE CashWithdrawal SET balance = 3 WHERE cash_withdrawal_id = 2;
+DELETE FROM CashWithdrawal WHERE cash_withdrawal_id = 2;
+
+-- 54. CashDeposit
+UPDATE CashDeposit SET balance = 4 WHERE cash_deposit_id = 1;
+DELETE FROM CashDeposit WHERE cash_deposit_id = 1;
+
+-- 55. CheckDeposit
+UPDATE CheckDeposit SET balance = 100 WHERE check_deposit_id = 1;
+DELETE FROM CheckDeposit WHERE check_deposit_id = 1;
+
+-- 56. CashProcess
+UPDATE CashProcess SET cash_withdrawal_id = 1 WHERE cash_process_id = 1;
+DELETE FROM CashProcess WHERE cash_process_id = 1;
+
